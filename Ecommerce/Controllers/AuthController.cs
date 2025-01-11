9466ec57f3c6814c9cae5fb652f7e0229f0e0c10
User <User@DESKTOP-94E2O8D>
2025-01-12 03:14:39 +0600

    Add project files.

---
+++ Ecommerce/Controllers/AuthController.cs
@@ -0,0 +1,65 @@
+﻿using Ecommerce.Model;
+using Ecommerce.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserService _userService;
+        public AuthController(UserService userservice)
+        {
+            _userService = userservice;
+        }
+        [HttpPost("register/Customer")]
+        public async Task<IActionResult> RegisterCustomer([FromBody] User user)
+        {
+            user.Role = "Customer";
+            await _userService.CreateUser(user);
+            return Ok();
+        }
+        [HttpPost("register/Admin")]
+        public async Task<IActionResult> RegisterAdmin([FromBody] User user)
+        {
+            user.Role = "Admin";
+            await _userService.CreateUser(user);
+            return Ok();
+        }
+        [HttpPost("login/Customer")]
+        public async Task<IActionResult> LoginCustomer([FromBody] User user)
+        {
+            var existingUser = await _userService.GetUser(user.Name, user.Password);
+            if (existingUser == null || existingUser.Role != "Customer")
+            {
+                return Unauthorized();
+            }
+            return Ok();
+        }
+        [HttpPost("login/Admin")]
+        public async Task<IActionResult> LoginAdmin([FromBody] User user)
+        {
+            var existingUser = await _userService.GetUser(user.Name, user.Password);
+            if (existingUser == null || existingUser.Role != "Admin")
+            {
+                return Unauthorized();
+            }
+            return Ok();
+        }
+        [HttpGet("all")]
+
+        public async Task<IActionResult> GetUsers()
+        {
+            var users = await _userService.GetUsers();
+            return Ok(users);
+        }
+
+
+
+
+
+
+    }
+}
