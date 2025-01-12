70e52004c5a2a815b7cb23d194b274801f8f1cb7
MD Shihabuzzaman <MD Shihabuzzaman@SHIHABUZZAMAN-P>
2025-01-12 19:51:53 +0600

    Add project files.

---
+++ Ecommerce/Controllers/CategoryController.cs
@@ -0,0 +1,48 @@
+﻿using Ecommerce.Model;
+using Ecommerce.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly CategoryService _categoryService;
+        public CategoryController(CategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetCategories()
+        {
+            var categories = await _categoryService.GetCategories();
+            return Ok(categories);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory([FromBody] Category category)
+        {
+            await _categoryService.CreateCategory(category);
+            return Ok();
+        }
+        [HttpPut("{name}")]
+        public async Task<IActionResult> UpdateCategory(string name, [FromBody] Category category)
+        {
+            await _categoryService.UpdateCategory(name, category);
+            return Ok();
+        }
+        [HttpDelete("{name}")]
+        public async Task<IActionResult> DeleteCategory(string name)
+        {
+            await _categoryService.DeleteCategory(name);
+            return Ok();
+        }
+
+
+
+
+
+
+    }
+}
