d73303e340b555c3846b3eeab242bd66170dd8f6
Shihabuzzaman <shihabuzzaman007@gmail.com>
2025-03-15 01:08:39 +0600

    Removed duplicate outer Ecommerce folder

--- Ecommerce/Model/MongoDBSettings.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Ecommerce.Model
-{
-    public class MongoDBSettings
-    {
-        public string ConnectionString { get; set; }
-        public string DatabaseName { get; set; }
-    }
-}
