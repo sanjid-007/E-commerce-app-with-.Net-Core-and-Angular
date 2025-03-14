2c8bc15c1fccf26381487e1bc84209020767180f
Shihabuzzaman <shihabuzzaman007@gmail.com>
2025-03-15 01:01:16 +0600

    Moved Ecommerce folder to backend

---
+++ backend/Ecommerce/Services/UserService.cs
@@ -0,0 +1,36 @@
+﻿using Ecommerce.Model;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace Ecommerce.Services
+{
+    public class UserService
+    {
+        private readonly IMongoCollection<User> _users;
+        public UserService(IOptions<MongoDBSettings> settings)
+        {
+            var client = new MongoClient(settings.Value.ConnectionString);
+            var database = client.GetDatabase(settings.Value.DatabaseName);
+            _users = database.GetCollection<User>("Users");
+        }
+        public async Task CreateUser(User user)
+        {
+            if (await _users.Find(u => u.Name == user.Name).FirstOrDefaultAsync() != null)
+            {
+                throw new Exception("User already exists");
+            }
+
+            await _users.InsertOneAsync(user);
+        }
+        public async Task<User> GetUser(string username, string password)
+        {
+            return await _users.Find(u => u.Name == username && u.Password == password).FirstOrDefaultAsync();
+        }
+        public async Task<List<User>> GetUsers()
+        {
+            return await _users.Find(u => true).ToListAsync();
+        }
+
+
+    }
+}
